a93ee010bafeed27e08c545cbc6f8265290f96e4
David Sousa <davidlsousa.dev@gmail.com>
2025-05-27 23:13:24 -0300

    chore(deps): implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -11,6 +13,15 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Expenses API')
+    .setDescription('Desafio tecnico!')
+    .setVersion('1.0')
+    .addTag('expenses_api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
